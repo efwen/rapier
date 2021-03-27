e6b4cc862042dddb37c19e7d71e58cb544f9ed60
Nick Carpenetti <nickcarpenetti@gmail.com>
2021-03-26 19:21:16 -0700

    moved win32window code to cpp file. conditional inclusion of platform files in cmake

--- src/platform/win32_window.cpp
+++ src/platform/win32_window.cpp
@@ -3,5 +3,16 @@
 #include "platform/win32_window.hpp"
 
 namespace rp {
+  Win32Window::Win32Window(std::string_view title, uint32_t width, uint32_t height) {
+    log::info("Creating Win32 Window!");
+    log::info("Title: {}, Resolution: {}x{} pixels", title, width, height);
+  }
 
+  Win32Window::~Win32Window() {
+    log::info("Destroying Win32 Window!");
+  } 
+
+  void Win32Window::setCallback(const std::function<void()>& callback) {
+    log::error("Win32Window setCallback() Function not implemented!");
+  }
 }
--- src/platform/win32_window.hpp
+++ src/platform/win32_window.hpp
@@ -8,17 +8,9 @@
 namespace rp {
   class Win32Window : public rp::Window {
     public:
-      Win32Window(std::string_view title, uint32_t width, uint32_t height) {
-        log::info("Creating Win32 Window!");
-        log::info("Title: {}, Resolution: {}x{} pixels", title, width, height);
-      }
+      Win32Window(std::string_view title, uint32_t width, uint32_t height);
+      ~Win32Window();
 
-      ~Win32Window() {
-        log::info("Destroying Win32 Window!");
-      } 
-
-      void setCallback(const std::function<void()>& callback) {
-        log::error("Win32Window setCallback() Function not implemented!");
-      }
+      void setCallback(const std::function<void()>& callback);
   };
 }
